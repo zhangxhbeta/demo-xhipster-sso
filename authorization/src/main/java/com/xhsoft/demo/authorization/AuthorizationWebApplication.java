a24b668f4dbf60459ded8eddb6961203d82c4d0a
张晓辉 <zhangxh@zhangxiaohuideMacBook-Pro.local>
2016-06-03 09:42:02 +0800

    配置好 ui 服务器
    ui 服务器通过 认证服务器 进行登录操作

--- authorization/src/main/java/com/xhsoft/demo/authorization/AuthorizationWebApplication.java
+++ authorization/src/main/java/com/xhsoft/demo/authorization/AuthorizationWebApplication.java
@@ -35,7 +35,7 @@ public class AuthorizationWebApplication {
         addDefaultProfile(app, source);
         Environment env = app.run(args).getEnvironment();
         log.info("Access URLs:\n----------------------------------------------------------\n\t" +
-                        "Local: \t\thttp://127.0.0.1:{}{}\n\t" +
+                        "Local: \t\thttp://localhost:{}{}\n\t" +
                         "External: \thttp://{}:{}{}\n----------------------------------------------------------",
                 env.getProperty("server.port"),
                 env.getProperty("server.contextPath"),
--- foo/src/main/java/com/xhsoft/demo/authorization/FooWebApplication.java
+++ foo/src/main/java/com/xhsoft/demo/authorization/FooWebApplication.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
@@ -14,6 +15,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class FooWebApplication {
 
     private static final Logger log = LoggerFactory.getLogger(FooWebApplication.class);
@@ -30,7 +32,7 @@ public class FooWebApplication {
         addDefaultProfile(app, source);
         Environment env = app.run(args).getEnvironment();
         log.info("Access URLs:\n----------------------------------------------------------\n\t" +
-                        "Local: \t\thttp://127.0.0.1:{}{}\n\t" +
+                        "Local: \t\thttp://localhost:{}{}\n\t" +
                         "External: \thttp://{}:{}{}\n----------------------------------------------------------",
                 env.getProperty("server.port"),
                 env.getProperty("server.contextPath"),
